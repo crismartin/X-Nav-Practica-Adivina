f4c0cdf33021f07252ae582cb84434e525fc793c
crismartin <cris.martinz.urjc@hotmail.es>
2015-05-18 00:13:44 +0200

    cambios leves

--- app.js
+++ app.js
@@ -50,5 +50,50 @@ function pedirFichero(fichero){
 $(document).ready(function(){	
 	pedirFichero("juegos/Capitales.json");	
 
+	var map = L.map('map').setView([51.505, -0.09], 13);
+
+	L.tileLayer('https://{s}.tiles.mapbox.com/v3/{id}/{z}/{x}/{y}.png', {
+		maxZoom: 18,
+		attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, ' +
+			'<a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
+			'Imagery © <a href="http://mapbox.com">Mapbox</a>',
+		id: 'examples.map-i875mjb7'
+	}).addTo(map);
+
+
+    // Show lat and long at cliked (event) point, with a popup
+    var popup = L.popup();
+    function showPopUp(e) {
+	popup
+            .setLatLng(e.latlng)
+            .setContent("Coordinates: " + e.latlng.toString())
+            .openOn(map);
+    }
+    // Subscribe to the "click" event
+    map.on('click', showPopUp);
+
+    // Show a circle around current location
+    function onLocationFound(e) {
+	var radius = e.accuracy / 2;
+	L.marker(e.latlng).addTo(map)
+            .bindPopup("You are within " + radius +
+		       " meters from this point<br/>" +
+		       "Coordinates: " + e.latlng.toString())
+	    .openPopup();
+	L.circle(e.latlng, radius).addTo(map);
+    }
+    // Subscribe to the "location found" event
+    map.on('locationfound', onLocationFound);
+
+    // Show alert if geolocation failed
+    function onLocationError(e) {
+	alert(e.message);
+    }
+    // Subscribe to the "location error" event
+    map.on('locationerror', onLocationError);
+
+    // Set the view to current location
+    map.locate({setView: true, maxZoom: 16});  
+
 	$("#selectmenu").selectmenu(); 
 });
